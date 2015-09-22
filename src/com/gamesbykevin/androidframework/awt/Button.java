0ca723d94a28d54754e0da119ae7bd30837a82bf
GOD <GOD@GOD-HP>
2015-09-22 00:04:27 -0500

    Changes to button
    Added ability to assign text for rendering. 
    Added function to place text in middle of the button, the purpose so we only need 1 image resource since all buttons will be similar

--- src/com/gamesbykevin/androidframework/awt/Button.java
+++ src/com/gamesbykevin/androidframework/awt/Button.java
@@ -2,6 +2,7 @@ package com.gamesbykevin.androidframework.awt;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.Rect;
 
 import com.gamesbykevin.androidframework.base.Entity;
@@ -22,6 +23,12 @@ public class Button extends Entity implements Disposable
     //is the button visible
     private boolean visible = true;
     
+    //the location of the button text
+    private float textX, textY;
+    
+    //the text to display on top of the button
+    private String text = "";
+    
     /**
      * Create a button
      * @param image Image representing button
@@ -36,6 +43,80 @@ public class Button extends Entity implements Disposable
         
         //set default bounds
         setBounds(0, 0, (int)getWidth(), (int)getHeight());
+    }
+    
+    /**
+     * Assign the text
+     * @param text The text we want to render on top of the button
+     */
+    public void setText(final String text)
+    {
+        this.text = text;
+    }
+    
+    /**
+     * Get the text
+     * @return The text we want to render on top of the button
+     */
+    public String getText()
+    {
+        return this.text;
+    }
+    
+    /**
+     * Position the text in the center of the button.<br>
+     * The position will depend on the (x,y) and (width, height)
+     * @param paint Object containing desired font metrics
+     */
+    public void positionText(final Paint paint)
+    {
+        //create temp rectangle object
+        Rect tmp = new Rect();
+        
+        //populate the rectangle dimensions based on the text and font metrics of our paint object
+        paint.getTextBounds(getText(), 0, getText().length(), tmp);
+        
+        //position the x-coordinate in the middle
+        setTextX((float)(getX() + (getWidth() / 2) - (tmp.width() / 2)));
+        
+        //position the y-coordinate in the middle
+        setTextY((float)(getY() + (getHeight() / 2) + (tmp.height() / 2)));
+    }
+    
+    /**
+     * Assign the text coordinate
+     * @param textX The desired x-coordinate where we want the text to display
+     */
+    public void setTextX(final float textX)
+    {
+        this.textX = textX;
+    }
+    
+    /**
+     * Assign the text coordinate
+     * @param textY The desired y-coordinate where we want the text to display
+     */
+    public void setTextY(final float textY)
+    {
+        this.textY = textY;
+    }
+    
+    /**
+     * Get the text coordinate
+     * @return The desired x-coordinate where we assigned the text to display
+     */
+    public float getTextX()
+    {
+        return this.textX;
+    }
+    
+    /**
+     * Get the text coordinate
+     * @return The desired y-coordinate where we assigned the text to display
+     */
+    public float getTextY()
+    {
+        return this.textY;
     }
     
     /**
@@ -118,6 +199,30 @@ public class Button extends Entity implements Disposable
         return getBounds().contains((int)x, (int)y);
     }
     
+    /**
+     * Render the button image with the assigned text
+     * @param canvas Object to write pixel data
+     * @param paint Paint object containing font metrics
+     * @throws Exception 
+     */
+    public void render(final Canvas canvas, final Paint paint) throws Exception
+    {
+        //if not visible don't render it
+        if (!isVisible())
+            return;
+        
+        //render the button
+        this.render(canvas);
+        
+        //now draw text on top of button
+        canvas.drawText(getText(), getTextX(), getTextY(), paint);
+    }
+    
+    /**
+     * Render just the button image itself
+     * @param canvas Object to write pixel data
+     * @throws Exception 
+     */
     @Override
     public void render(final Canvas canvas) throws Exception
     {
